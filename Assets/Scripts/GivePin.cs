c763ca1cac6ec111a220dca15f7791bde346dfe5
fememism <37868407+fememism@users.noreply.github.com>
2024-04-15 19:06:08 +0300

    fixes

---
+++ Assets/Scripts/GivePin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GivePin : MonoBehaviour
+{
+    public DiceBehaviour parentDice;
+    public void Trick()
+    {
+        if (parentDice.result==1)
+        {
+            parentDice.result=1;
+            FindObjectOfType<PinSystem>().ChangePinCount(1);
+            parentDice.DestroyDice();
+        }
+    }
+
+}
